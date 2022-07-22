c961d89196d4a01f71b3b71b811f02b8916905da
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-07-21 21:08:37 -0300

    feat: adjuste text color

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,10 +5,10 @@ interface IInput extends InputProps {}
 export const Input = ({ ...props }: IInput) => {
   return (
     <ChakraInput
-      _placeholder={{ color: "gray.600" }}
+      _placeholder={{ color: "gray.700" }}
       variant="flushed"
       borderColor="gray.800"
-      color="gray.300"
+      color="gray.500"
       {...props}
     />
   );
